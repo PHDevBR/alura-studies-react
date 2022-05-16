02ad0fde6602c435ad955b818d449bccaad426e3
phdevbr <pedroheringer2020@gmail.com>
2022-05-16 17:15:25 -0300

    Learning some basic styling

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,7 +3,9 @@ import React from "react";
 class Button extends React.Component {
     render() {
         return (
-            <button>
+            <button style={{
+                backgroundColor: 'blue',
+            }}>
                 Button
             </button>
         )
